51e9e4ec82c41908cefb0c7d639f4ac1b314a483
Chris J. Karr <chris@audacious-software.com>
2021-06-02 15:53:21 +0000

    Django 2 fixes

--- circle_settings.py
+++ circle_settings.py
@@ -30,13 +30,13 @@ INSTALLED_APPS = (
     'simple_messaging'
 )
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
 )
