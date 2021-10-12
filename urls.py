4b79e332993928b154e60e29451d7b137315116e
Chris J. Karr <chris@audacious-software.com>
2021-10-12 18:38:26 +0000

    Added legacy path for exising installs

--- urls.py
+++ urls.py
@@ -4,4 +4,5 @@ from .views import incoming_message_request
 
 urlpatterns = [
     url(r'^incoming$', incoming_message_request, name='incoming_message'),
+    url(r'^incoming.xml$', incoming_message_request, name='incoming_message_xml'),
 ]
