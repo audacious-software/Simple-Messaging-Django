c6606f83537c0bc3feaa643208ce23a3afbb511f
Chris J. Karr <chris@audacious-software.com>
2023-01-03 20:35:34 +0000

    Exposing ID on messages for cases of empty destinations

--- admin.py
+++ admin.py
@@ -49,9 +49,9 @@ mark_error_handled.short_description = "Mark error handled"
 @admin.register(OutgoingMessage)
 class OutgoingMessageAdmin(admin.ModelAdmin):
     if hasattr(settings, 'SIMPLE_MESSAGING_SHOW_ENCRYPTED_VALUES') and settings.SIMPLE_MESSAGING_SHOW_ENCRYPTED_VALUES:
-        list_display = ('current_destination', 'reference_id', 'send_date', 'sent_date', 'current_message', 'errored')
+        list_display = ('id', 'current_destination', 'reference_id', 'send_date', 'sent_date', 'current_message', 'errored')
     else:
-        list_display = ('destination', 'reference_id', 'send_date', 'sent_date', 'message', 'errored')
+        list_display = ('id', 'destination', 'reference_id', 'send_date', 'sent_date', 'message', 'errored')
 
     search_fields = ('destination', 'message', 'transmission_metadata',)
     list_filter = ('errored', 'send_date', 'sent_date',)
@@ -74,9 +74,9 @@ class OutgoingMessageAdmin(admin.ModelAdmin):
 @admin.register(IncomingMessage)
 class IncomingMessageAdmin(admin.ModelAdmin):
     if hasattr(settings, 'SIMPLE_MESSAGING_SHOW_ENCRYPTED_VALUES') and settings.SIMPLE_MESSAGING_SHOW_ENCRYPTED_VALUES:
-        list_display = ('current_sender', 'recipient', 'receive_date', 'current_message')
+        list_display = ('id', 'current_sender', 'recipient', 'receive_date', 'current_message')
     else:
-        list_display = ('sender', 'recipient', 'receive_date', 'message')
+        list_display = ('id', 'sender', 'recipient', 'receive_date', 'message')
 
     search_fields = ('sender', 'message',)
     list_filter = ('receive_date',)
