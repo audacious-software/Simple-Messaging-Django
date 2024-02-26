e36728a43177d3d6026f7b06227fe9962f25da7b
Chris Karr <chris@audacious-software.com>
2024-02-26 17:19:18 -0600

    Update urls.py

--- urls.py
+++ urls.py
@@ -8,15 +8,15 @@ from .views import incoming_message_request, simple_messaging_ui, simple_messagi
 if sys.version_info[0] > 2:
     from django.urls import include, re_path
 
-  urlpatterns = [
-      re_path(r'^incoming$', incoming_message_request, name='incoming_message'),
-      re_path(r'^incoming.xml$', incoming_message_request, name='incoming_message_xml'),
-      re_path(r'^console$', simple_messaging_ui, name='simple_messaging_ui'),
-      re_path(r'^messages.json$', simple_messaging_messages_json, name='simple_messaging_messages_json'),
-      re_path(r'^send.json$', simple_messaging_send_json, name='simple_messaging_send_json'),
-      re_path(r'^lookup$', simple_messaging_lookup, name='simple_messaging_lookup'),
-      re_path(r'^lookup.json$', simple_messaging_lookup_json, name='simple_messaging_lookup_json'),
-  ]
+    urlpatterns = [
+        re_path(r'^incoming$', incoming_message_request, name='incoming_message'),
+        re_path(r'^incoming.xml$', incoming_message_request, name='incoming_message_xml'),
+        re_path(r'^console$', simple_messaging_ui, name='simple_messaging_ui'),
+        re_path(r'^messages.json$', simple_messaging_messages_json, name='simple_messaging_messages_json'),
+        re_path(r'^send.json$', simple_messaging_send_json, name='simple_messaging_send_json'),
+        re_path(r'^lookup$', simple_messaging_lookup, name='simple_messaging_lookup'),
+        re_path(r'^lookup.json$', simple_messaging_lookup_json, name='simple_messaging_lookup_json'),
+    ]
 else:
     from django.conf.urls import include, url
   
