b44b5d13ecb93c2b8a37f5745a6ca9c1857af814
Chris J. Karr <chris@audacious-software.com>
2024-05-16 23:13:43 +0000

    Circle CI updates

--- .circleci/circle_settings.py
+++ .circleci/circle_settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',
+    'quicksilver',
     'simple_messaging',
     'simple_data_export',
 )
