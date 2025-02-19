42e8e1bd93b2ecce4461db97c1abbe1938235f91
Chris J. Karr <chris@audacious-software.com>
2025-02-19 23:47:03 +0000

    Code cleanup

--- management/commands/simple_messaging_fetch_short_urls.py
+++ management/commands/simple_messaging_fetch_short_urls.py
@@ -18,7 +18,7 @@ from quicksilver.decorators import handle_lock, handle_schedule, add_qs_argument
 
 from ...decorators import log_scheduled_event
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger(__name__) # pylint: disable=invalid-name
 
 class Command(BaseCommand):
     help = 'Diagnostic command for retrieving short URLs created by this client'
